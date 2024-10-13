298fbe47fa28472605863f2b7c0a7190065cc37d
waikyjung <waiky.jung@gmail.com>
2024-10-13 04:41:03 +0000

    rent working

--- blocksmashers.rb
+++ blocksmashers.rb
@@ -1,11 +1,12 @@
 require_relative "./bs_inventory"
 class Rental
-  attr_accessor :title, :year, :item_type, :stock
-  def initialize(title, year, item_type, stock)
+  attr_accessor :title, :year, :item_type, :available, :rented
+  def initialize(title, year, item_type, available, rented)
     @title = title
     @year = year
     @item_type = item_type
-    @stock = stock
+    @available = available
+    @rented = rented
   end
 end
 
@@ -27,19 +28,34 @@ class Blocksmashers
     num = 0
     @inventory.each do |item| 
       num += 1
-      puts "#{num}) [#{item.item_type}] #{item.title} (#{item.year}) - Available: #{item.stock}"
+      puts "#{num}) [#{item.item_type}] #{item.title} (#{item.year}) - Available: #{item.available} - Rented: #{item.rented}"
     end
   end
 
-  def add_title(title, year, item_type, stock)
-    @inventory.push Rental.new(title, year, item_type, stock)
+  def add(title, year, item_type, available, rented)
+    @inventory.push Rental.new(title, year, item_type, available, rented = 0)
     self.sort_titles
   end
 
-  def remove_title(title, year)
+  def remove(num)
+    @inventory.delete_at(num)
+    self.sort_titles
   end
+
+  def rent(num)
+    if @inventory[num].available > 0
+      @inventory[num].available -= 1
+      @inventory[num].rented += 1
+      return true
+    else
+      return false
+    end
+  end
+
+  
 end
 
+system("clear")
 x = Blocksmashers.new
-
+puts x.rent(0) == true ? "rented!" : "denied"
 x.display
--- bs_inventory.rb
+++ bs_inventory.rb
@@ -1,10 +1,10 @@
 $starting_inventory = [
-  ["Batman", 1990, "DVD", 5],
-  ["Batman Returns", 1992, "DVD", 5],
-  ["Teenage Mutant Ninja Turtles", 1987, "VHS", 5],
-  ["Forrest Gump", 1994, "VHS", 5],
-  ["The Fast & The Furious 23: Seniors on Patrol", 2028, "DVD", 1],
-  ["Super Mario Bros.", 1985, "Videogame", 5],
-  ["Tetris", 1988, "Videogame", 5],
-  ["Minecraft", 2011, "Videogame", 5]
+  ["Batman", 1990, "DVD", 5, 0],
+  ["Batman Returns", 1992, "DVD", 5, 0],
+  ["Teenage Mutant Ninja Turtles", 1987, "VHS", 5, 0],
+  ["Forrest Gump", 1994, "VHS", 5, 0],
+  ["The Fast & The Furious 23: Seniors on Patrol", 2028, "DVD", 1, 0],
+  ["Super Mario Bros.", 1985, "Videogame", 5, 0],
+  ["Tetris", 1988, "Videogame", 5, 0],
+  ["Minecraft", 2011, "Videogame", 5, 0]
 ]
